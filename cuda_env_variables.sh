f3b992ca1079c93a7c4ad98d3ec4da8a7a89155d
Vegard Wærp <vegardw@gmail.com>
2023-03-18 13:37:36 +0100

    Update cuda_env_variables.sh

--- cuda_env_variables.sh
+++ cuda_env_variables.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
 export CUDA_HOME=/usr/local/cuda
+export CUDA_VERSION=117
 export PATH="/usr/local/cuda/bin:$PATH"
