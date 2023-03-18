62283284f4eb04b5c20f0182851abfcc731ebc88
Vegard Wærp <vegardw@gmail.com>
2023-03-18 13:24:27 +0100

    Update setup_env_lambdalabs_vm.sh

--- setup_env_lambdalabs_vm.sh
+++ setup_env_lambdalabs_vm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Add nvidia public keys and repository
-sudo apt-key adv -y --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo apt-key adv -y --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-sudo add-apt-repository -y "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 
 # Pin Nvidia packages
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
